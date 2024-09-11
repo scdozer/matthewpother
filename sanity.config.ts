8a56de9f5fd9c2d8318b72b8e3770c2b49b03407
IB - Scott <scott@impossible-bureau.com>
2024-09-11 15:14:52 -0600

    build sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,15 +3,12 @@ import { structureTool } from "sanity/structure";
 
 import * as schemaTypes from "./sanity/schema";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-
 export default defineConfig({
   basePath: "/studio",
   title: "Mattew Pothier",
   apiVersion: "2024-05-21",
-  projectId,
-  dataset,
+  projectId: "bxcwz78t",
+  dataset: "production",
   plugins: [structureTool()],
   schema: {
     types: Object.values(schemaTypes),
