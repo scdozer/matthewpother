04a642ab8bb1f666797edb2690f2fba75f2ee77b
IB - Scott <scott@impossible-bureau.com>
2024-10-08 15:00:57 -0600

    updates

--- app/components/Project/ClientGallery.tsx
+++ app/components/Project/ClientGallery.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { SanityImageAsset, SanityFileAsset } from "@/sanity/utils/graphql";
+import ReactPlayer from "react-player/lazy";
 import styles from "./style.module.scss";
 
 gsap.registerPlugin(ScrollTrigger);
 
 interface ProjectGalleryProps {
   gallery: any;
-  videoEmbed?: any;
+  videoEmbed?: string;
 }
 
 const ProjectGallery = ({ gallery, videoEmbed }: ProjectGalleryProps) => {
@@ -17,16 +18,16 @@ const ProjectGallery = ({ gallery, videoEmbed }: ProjectGalleryProps) => {
 
   useEffect(() => {
     if (galleryRef.current) {
-      const images = gsap.utils.toArray(
+      const items = gsap.utils.toArray(
         galleryRef.current.querySelectorAll(`.${styles.galleryItem}`)
       );
 
-      images.forEach((image: any, index: number) => {
+      items.forEach((item: any, index: number) => {
         const isEven = index % 2 === 0;
         const startPosition = isEven ? "20%" : "-20%";
 
         gsap.fromTo(
-          image,
+          item,
           {
             y: startPosition,
             opacity: 0,
@@ -39,9 +40,9 @@ const ProjectGallery = ({ gallery, videoEmbed }: ProjectGalleryProps) => {
             duration: 1,
             ease: "power2.out",
             scrollTrigger: {
-              trigger: image,
+              trigger: item,
               start: "top bottom-=100",
-              end: "bottom top+=100",
+              end: "top middle",
               scrub: 1,
             },
           }
@@ -52,7 +53,7 @@ const ProjectGallery = ({ gallery, videoEmbed }: ProjectGalleryProps) => {
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
-  }, [gallery]);
+  }, [gallery, videoEmbed]);
 
   const isImageAsset = (item: any): item is { asset: SanityImageAsset } => {
     return item.asset && item.asset._type === "sanity.imageAsset";
@@ -66,48 +67,72 @@ const ProjectGallery = ({ gallery, videoEmbed }: ProjectGalleryProps) => {
     <div ref={galleryRef} className={styles.galleryContainer}>
       {videoEmbed && (
         <div className={`${styles.galleryItem} ${styles.videoItem}`}>
-          <video
-            src={videoEmbed}
-            autoPlay
-            loop
-            muted
-            playsInline
-            className={styles.video}
+          <ReactPlayer
+            url={videoEmbed}
+            width="100%"
+            height="100%"
+            playing={false}
+            loop={false}
+            muted={false}
+            playsinline={true}
+            controls={true}
+            config={{
+              youtube: {
+                playerVars: {
+                  modestbranding: 1,
+                  controls: 1,
+                  autoplay: 0,
+                  showinfo: 0,
+                  rel: 0,
+                },
+              },
+              vimeo: {
+                playerOptions: {
+                  background: false,
+                  autoplay: false,
+                  controls: true,
+                  responsive: true,
+                  dnt: true,
+                },
+              },
+            }}
           />
         </div>
       )}
-      {gallery?.map((item, index) => (
-        <div
-          key={index}
-          className={`${styles.galleryItem} ${
-            index % 3 === 0
-              ? styles.large
-              : index % 3 === 1
-                ? styles.medium
-                : styles.small
-          }`}
-        >
-          {isImageAsset(item) && (
-            <Image
-              src={item?.asset?.url || ""}
-              alt={`Project image ${index + 1}`}
-              width={item?.asset?.metadata?.dimensions?.width || 1000}
-              height={item?.asset?.metadata?.dimensions?.height || 1000}
-              layout="responsive"
-              objectFit="cover"
-            />
-          )}
-          {isFileAsset(item) && (
-            <a
-              href={item?.asset?.url || ""}
-              download
-              className={styles.fileDownload}
-            >
-              {item.asset.originalFilename}
-            </a>
-          )}
-        </div>
-      ))}
+      {gallery?.map(
+        (item: SanityImageAsset | SanityFileAsset, index: number) => (
+          <div
+            key={index}
+            className={`${styles.galleryItem} ${
+              index % 3 === 0
+                ? styles.large
+                : index % 3 === 1
+                  ? styles.medium
+                  : styles.small
+            }`}
+          >
+            {isImageAsset(item) && (
+              <Image
+                src={item?.asset?.url || ""}
+                alt={`Project image ${index + 1}`}
+                width={item?.asset?.metadata?.dimensions?.width || 1000}
+                height={item?.asset?.metadata?.dimensions?.height || 1000}
+                layout="responsive"
+                objectFit="cover"
+              />
+            )}
+            {isFileAsset(item) && (
+              <a
+                href={item?.asset?.url || ""}
+                download
+                className={styles.fileDownload}
+              >
+                {item.asset.originalFilename}
+              </a>
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 };
